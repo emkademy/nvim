a46e09547c79f1732fc892fa7993c38cadd0f993
Kıvanç Yüksel <emkademy@gmail.com>
2023-10-09 22:42:40 +0200

    added code action for type: ignore

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,6 +5,25 @@ return {
     dependencies = { "mason.nvim" },
     opts = function()
       local nls = require("null-ls")
+
+      nls.register({
+        name = "add type ignore",
+        method = { require("null-ls").methods.CODE_ACTION },
+        filetypes = { "_all" },
+        generator = {
+          fn = function()
+            return {
+              {
+                title = "ignore type annotations",
+                action = function()
+                  vim.cmd("normal $A  # type: ignore")
+                end,
+              },
+            }
+          end,
+        },
+      })
+
       return {
         root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
         sources = {
@@ -14,6 +33,7 @@ return {
           nls.builtins.formatting.shfmt,
           nls.builtins.diagnostics.mypy,
           nls.builtins.diagnostics.ruff,
+          -- nls.builtins.diagnostics.pyright,
           -- nls.builtins.diagnostics.flake8,
         },
       }
