5377f2a0ccb93209471c4b8aaf6137dcb8d3de99
Kıvanç Yüksel <83173240+emkademy@users.noreply.github.com>
2024-07-16 09:16:37 +0200

    Update disable.lua

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,8 +1,4 @@
 return {
   { "jose-elias-alvarz/null-ls.nvim", enabled = false },
   { "nvim-notify", enabled = false },
-  { "preservim/nerdtree", enabled = false },
-  { "github/copilot.vim", enabled = false },
-  -- { "airblade/vim-rooter", enabled = false },
-  { "notjedi/nvim-rooter", enabled = false },
 }
