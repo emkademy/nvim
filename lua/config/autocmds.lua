2a96c9f0451c0796a62cb95aac34c82640cfaf89
Kıvanç Yüksel <emkademy@gmail.com>
2024-09-18 08:02:01 +0200

    updated packages

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/artificial intelligence/autocmds.lua
 -- Add any additional autocmds here
+
+-- turn-off continuation of comments when pressing Enter
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "*",
+  callback = function()
+    vim.cmd("setlocal formatoptions-=cro")
+  end,
+})
