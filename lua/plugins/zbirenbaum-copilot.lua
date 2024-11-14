2fe06317e035350eb41c78517917ba29f8d731f6
Kivanc Yuksel <kivanc.yuksel@allegro.com>
2024-11-14 06:00:25 +0100

    updated copilot settings

--- lua/plugins/zbirenbaum-copilot.lua
+++ lua/plugins/zbirenbaum-copilot.lua
@@ -4,8 +4,21 @@ return {
     cmd = "Copilot",
     build = ":Copilot auth",
     opts = {
-      panel = { enabled = true, auto_refresh = true },
-      suggestion = { enabled = true, auto_trigger = true },
+      panel = { enabled = false, auto_refresh = true },
+      suggestion = {
+        enabled = false,
+        auto_trigger = true,
+        hide_during_completion = true,
+        debounce = 75,
+        keymap = {
+          accept = "<M-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
       filetypes = {
         markdown = true,
         help = true,
