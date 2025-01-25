10770abe5be078fe76c568a4c4a47d01f3de002b
Kıvanç Yüksel <emkademy@gmail.com>
2025-01-25 10:08:37 +0100

    updated dashboard

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -2,6 +2,7 @@ return {
   { "jose-elias-alvarz/null-ls.nvim", enabled = false },
   { "nvim-notify", enabled = false },
   { "image.nvim", enabled = false },
+  { "mini.animate", enabled = false },
   -- { "noice.nvim", enabled = false },
   -- { "nitifier.nvim", enabled = false },
 }
